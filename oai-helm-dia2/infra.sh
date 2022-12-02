6e118d5ed30fb8c90605f41da10a7c4afd11c216
Sergio Tarazona <starazona@whitestack.com>
2022-12-02 00:57:47 +0000

    agregando EIR

--- oai-helm-dia2/infra.sh
+++
@@ -1,4 +0,0 @@
-# Add PDU, replace "etsi-openstack" for the name of the VIM you're using
-VIMID=`osm vim-list | grep "etsi-openstack " | awk '{ print $4 }'`
-sed -i "s/vim_accounts: .*/vim_accounts: [ $VIMID ]/" pdu.yaml
-osm pdu-create --descriptor_file pdu.yaml
--- oai-helm-dia2/vyos_pnf/helm-charts/eechart/source/install_nginx.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -eux
-sudo -s <<EOF
-apt update
-apt install -y nginx
-systemctl status nginx
-EOF
-
